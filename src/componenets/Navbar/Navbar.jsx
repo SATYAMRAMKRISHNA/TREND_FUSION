0461ad64c73bd4f06407b07d9ed922d1a3c6b876
SATYAMRAMKRISHNA <141958121+SATYAMRAMKRISHNA@users.noreply.github.com>
2025-03-25 01:53:25 -0700

    Update Navbar.jsx

--- src/componenets/Navbar/Navbar.jsx
+++ src/componenets/Navbar/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
         <li onClick={()=>{setMenu("shop")}}><Link style={{textDecoration:'none'}} to='/'>Shop</Link>{menu ==="shop"?<hr/>:<></>}</li>
         <li onClick={()=>{setMenu("mens")}}><Link style={{textDecoration:'none'}} to='/mens'>Men</Link>{menu ==="mens"?<hr/>:<></>}</li>
         <li onClick={()=>{setMenu("womens")}}><Link style={{textDecoration:'none'}} to='/womens'>Women</Link>{menu ==="womens"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("kids")}}><Link to='/kids'>Kids</Link>{menu ==="kids"?<hr/>:<></>}</li>
+       
         <li onClick={()=>{setMenu("connect")}}><Link style={{textDecoration:'none'}} to='/connect'>Connect</Link>{menu ==="connect"?<hr/>:<></>}</li>
       </ul>
       <div className="nav-login-cart">
